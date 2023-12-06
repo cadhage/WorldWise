523a0a2d397a40b71b2961b8a513dc6aa72b5d88
cadhage <cadhage@gmail.com>
2023-12-07 00:34:43 +0530

    updating file for router

--- WorldWise/src/App.jsx
+++ WorldWise/src/App.jsx
@@ -1,6 +1,19 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from "./pages/Product";
+import HomePage from "./pages/HomePage";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
-  return <div>WorldWise</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App;
